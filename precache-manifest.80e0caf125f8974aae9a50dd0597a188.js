d14c14a7b587a0619e743cbcd878e8afaa3504bc
akshaymadlur2 <akshaymadlur2@gmail.com>
2021-05-10 19:26:55 +0530

    Updates

---
+++ precache-manifest.80e0caf125f8974aae9a50dd0597a188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed599f6910f6fe61899b9440c892eff6",
+    "url": "/happy-travel/index.html"
+  },
+  {
+    "revision": "608054eb24b0c224900a",
+    "url": "/happy-travel/static/css/main.b3209779.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/happy-travel/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/happy-travel/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608054eb24b0c224900a",
+    "url": "/happy-travel/static/js/main.60fc7618.chunk.js"
+  },
+  {
+    "revision": "8498fce5eed1e314b129",
+    "url": "/happy-travel/static/js/runtime-main.11c3867f.js"
+  }
+]);
